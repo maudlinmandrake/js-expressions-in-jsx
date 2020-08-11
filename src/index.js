89bd4d5a1de9236919bc93182d3f8df193bb07bc
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-11 11:08:46 -0400

    Wee! Concatenate strings!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Jenny";
+const fName = "Jenny";
+const lName = "Mikac";
 const luckyNumber = 9;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {fName + " " + lName}!</h1>
     <p>My lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
