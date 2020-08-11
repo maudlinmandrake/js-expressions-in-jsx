6e9134adc3a1ed9b31328a1ac044174669e36fd7
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-11 11:06:47 -0400

    Put a JS expression into the curly braces instead of a const JSX will interpret any JavaScript expressions you put into the curly braces, so you can put in simple things like a math equation or functions like Math.random/Math.floor. It will not work with statements (if/else, switch)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const luckyNumber = 9;
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
-    <p>My lucky number is {luckyNumber}</p>
+    <p>My lucky number is {Math.floor(Math.random()) * 10}</p>
   </div>,
   document.getElementById("root")
 );
+
+//You can put any JS expression into the curly braces and the code will be compiled within your HTML
+//This will not work with JS statements (ie if/else)
